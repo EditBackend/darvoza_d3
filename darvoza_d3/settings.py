ffadeb21d4a671586cda10fe9719cb05c178c4fc
EditBackend <commanderx002@gmail.com>
2026-05-14 15:39:40 +0500

    eng so'nggi hato to'g'rilandi Diyorbck1 loyiha yakuniga yetdi Inshaalloh

--- darvoza_d3/settings.py
+++ darvoza_d3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,3 +143,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Media fayllar uchun sozlamalar
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
